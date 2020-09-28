94e019a2e476add34f17b09da7f52f924586b25b
Andrea Tchalaou <attchalaou@gmail.com>
2020-09-28 07:41:09 -0400

    added get/render and created 3 form pages

--- 2dooer-app/app/controllers/lists_controller.rb
+++ 2dooer-app/app/controllers/lists_controller.rb
@@ -1,3 +1,12 @@
 class ListsController < ApplicationController
 
+    # get lists/new to render a form to create new entry. GET request will show something
+    # its job is to render a page
+    get '/lists/new' do
+        erb
+    end
+    # post lists to create a new list
+    # show page for a list
+    # index route for all lists
+   
 end
