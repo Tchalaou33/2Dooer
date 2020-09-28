d01610d9c38a41c3f8cbd9c1235a691c0b27cffb
Andrea Tchalaou <attchalaou@gmail.com>
2020-09-28 07:46:16 -0400

    route to render new form

--- 2dooer-app/app/controllers/lists_controller.rb
+++ 2dooer-app/app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
     # get lists/new to render a form to create new entry. GET request will show something
     # its job is to render a page
     get '/lists/new' do
-        erb
+        erb :'/lists/new'
     end
     # post lists to create a new list
     # show page for a list
