c61561b336298e569fdbaa5e4fa1da06d9b8bc58
Andrea Tchalaou <attchalaou@gmail.com>
2020-10-02 15:54:19 -0400

    add post lists

--- 2dooer-app/app/controllers/lists_controller.rb
+++ 2dooer-app/app/controllers/lists_controller.rb
@@ -6,6 +6,25 @@ class ListsController < ApplicationController
         erb :'lists/new'
      end
     # post lists to create a new list (redirect)
+    post '/lists' do
+        # I want to create a new list and save it to the DB
+        # I only want to save list if it has some content
+        # Only want to create a list if user is logged in
+        if !logged_in?
+            redirect '/'
+        end
+
+        if params[:content] !=""
+            #create a new list
+            @list = List.create(content: params[:content],
+            user_id: current_user.id)
+            redirect "/lists/#{@list.id}"
+
+        else
+            redirect '/lists/new'
+        end
+    end
+
     # show page for a list
     # index route for all lists
 end
