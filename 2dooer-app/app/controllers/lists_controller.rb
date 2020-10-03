38b81bdf9bf551c15a077a75b03001c42b5f0992
Andrea Tchalaou <attchalaou@gmail.com>
2020-10-03 12:11:46 -0400

    additional

--- 2dooer-app/app/controllers/lists_controller.rb
+++ 2dooer-app/app/controllers/lists_controller.rb
@@ -28,14 +28,32 @@ class ListsController < ApplicationController
     # show page for a list
     # dynamic route/ 
     get '/lists/:id' do
-        @list = List.find(params[:id])
+        set_list
         erb :'lists/show'
     end
 
     # This route should send us to lists/edit.erb, which will
     # render a edit form
     get '/lists/:id/edit' do
+        set_list
         erb :'/lists/edit'
       end
+
+      # This action's job is to ...??
+      patch '/lists/:id' do
+        #1. find the list 
+        set_list
+        #2. modify (update) list
+        @list.update(content: params[:content])
+        #3. redirect to show page
+        redirect "/lists/#{@list.id}"
+      end
     # index route for all lists
+
+    private
+
+    def set_list
+        @list = List.find(params[:id])  
+    end
+
 end
